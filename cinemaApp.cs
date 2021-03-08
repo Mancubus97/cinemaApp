77a4a4edfc0d656f692f65c6c396bf09793696e2
FurkanK <73836618+Furkan1000174@users.noreply.github.com>
2021-03-08 16:36:41 +0100

    Test

--- cinemaApp.cs
+++ cinemaApp.cs
@@ -1,1 +1,14 @@
-hallo
+using System;
+
+namespace CinemaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}.");
+        }
+    }
+}
